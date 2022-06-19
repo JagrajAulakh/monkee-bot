32106433c1f088899bbc6acf6236b9b695718bf9
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-06-19 15:26:43 -0400

    Added ping slash command

--- bot.js
+++ bot.js
@@ -8,7 +8,15 @@ client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}`);
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
 
 
 console.log(`TOKEN: ${process.env.DISCORD_BOT_TOKEN}`);
---
+++ deploy-commands.js
@@ -0,0 +1,33 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+require('dotenv').config();
+
+const { DISCORD_BOT_CLIENT_ID, DISCORD_BOT_GUILD_ID, DISCORD_BOT_TOKEN} = process.env;
+
+console.log(DISCORD_BOT_CLIENT_ID, DISCORD_BOT_GUILD_ID, DISCORD_BOT_TOKEN);
+
+const commands = [
+	{
+		"name": 'ping',
+		"description": 'Replies with pong!',
+	},
+	{
+		"name": "joe",
+		"description": "joe",
+		"options": [
+			{
+				"type": 3,
+				"name": "input",
+				"description": "input",
+				"required": true
+			}
+		]
+	}
+]
+
+const rest = new REST({ version: '9' }).setToken(DISCORD_BOT_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(DISCORD_BOT_CLIENT_ID, DISCORD_BOT_GUILD_ID), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
