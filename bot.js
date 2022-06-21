483105df362ece1323efb2c4689c15f1b3546271
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-06-21 18:12:35 -0400

    Added Dad bot functionality

--- bot.js
+++ bot.js
@@ -2,16 +2,35 @@ const { Client, Intents } = require("discord.js");
 const { ROASTS } = require("./roasts.js");
 require("dotenv").config();
 
+// Delcare all intents that the bot has
 const client = new Client({
 	intents: [
 		Intents.FLAGS.GUILDS,
 		Intents.FLAGS.GUILD_PRESENCES,
+		Intents.FLAGS.GUILD_MESSAGE_TYPING,
 		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.DIRECT_MESSAGES,
 	],
 });
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", async (msg) => {
+	let match;
+	if (
+		msg.author.id !== client.user.id &&
+		(match = msg.content.match("[iI]'?[mM]\\s"))
+	) {
+		// Find where the regex matched (it'll get the starting of the match) and add
+		// length of matched term. That will start the substr from after "I'm"
+		const everythingElse = msg.content.substring(
+			match.index + match[0].length
+		);
+		msg.channel.send(`Hi ${everythingElse}, I'm Monkee Bot!`);
+	}
 });
 
 client.on("interactionCreate", async (interaction) => {
