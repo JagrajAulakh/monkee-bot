0424ecc67cdafb5c45e482aed3cb90546a3d6d2c
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-06-20 22:49:38 -0400

    Formatting and added /roast command :imp:

--- bot.js
+++ bot.js
@@ -1,28 +1,38 @@
-const { Client, Intents } = require('discord.js');
-require('dotenv').config();
+const { Client, Intents } = require("discord.js");
+const { ROASTS } = require("./roasts.js");
+require("dotenv").config();
 
-
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS]});
+const client = new Client({
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_PRESENCES,
+		Intents.FLAGS.GUILD_MEMBERS,
+	],
+});
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async (interaction) => {
 	if (!interaction.isCommand()) return;
 
 	const { commandName } = interaction;
 
-	if (commandName === 'ping') {
-		console.log("Revieved command: ping");
-		await interaction.reply('Pong!');
-	}
-	else if (commandName === 'joe') {
-		console.log("Revieved command: joe");
-		await interaction.reply('GOTEM!');
+	if (commandName === "ping") {
+		console.log("Received command: ping");
+		await interaction.reply("Pong!");
+	} else if (commandName === "roast") {
+		console.log("Received command: roast");
+		const user = interaction.options.getUser("username");
+		await interaction.reply(
+			`<@${interaction.options.get("username").value}> ${
+				ROASTS[Math.floor(Math.random() * ROASTS.length)]
+			}`
+		);
+
 	}
 });
-
 
 console.log(`TOKEN: ${process.env.DISCORD_BOT_TOKEN}`);
 client.login(process.env.DISCORD_BOT_TOKEN);
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,31 +1,30 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-require('dotenv').config();
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+require("dotenv").config();
 
-const { DISCORD_BOT_CLIENT_ID, DISCORD_BOT_GUILD_ID, DISCORD_BOT_TOKEN} = process.env;
+const { DISCORD_BOT_CLIENT_ID, DISCORD_BOT_TOKEN } = process.env;
 
 const commands = [
 	{
-		"name": 'ping',
-		"description": 'Replies with pong!',
+		name: "ping",
+		description: "Replies with pong!",
 	},
 	{
-		"name": "joe",
-		"description": "joe",
-		"options": [
+		name: "roast",
+		description: "Roast a mf in the chat",
+		options: [
 			{
-				"type": 3,
-				"name": "input",
-				"description": "input",
-				"required": true
-			}
-		]
-	}
-]
+				type: 6,
+				name: "username",
+				description: "Username of the mf you want to roast",
+				required: true,
+			},
+		],
+	},
+];
 
-const rest = new REST({ version: '9' }).setToken(DISCORD_BOT_TOKEN);
+const rest = new REST({ version: "9" }).setToken(DISCORD_BOT_TOKEN);
 
 rest.put(Routes.applicationCommands(DISCORD_BOT_CLIENT_ID), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
+	.then(() => console.log("Successfully registered application commands."))
 	.catch(console.error);
-
